ab70da97ef94038d3ad9a4a183dc7f9da532b9c8
ponowemu <m.ferfet@ponowemu.pl>
2020-01-22 22:44:21 +0100

    Another model added

---
+++ Negative-Association-Rules-Miner/Negative-Association-Rules-Miner/model/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negative_Association_Rules_Miner.model
+{
+    /// <summary>
+    /// Single item model 
+    /// </summary>
+    class Item
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Negative-Association-Rules-Miner/Negative-Association-Rules-Miner/model/Rule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negative_Association_Rules_Miner.model
+{
+    /// <summary>
+    /// Basic model of single association rule 
+    /// </summary>
+    class Rule
+    {
+        public int TransactionId { get; set; }
+        public IEnumerable<Item> ItemSet
+        {
+            get { return ItemSet.OrderBy(i => i.Name); }
+            set { ItemSet = value; } 
+        }
+
+    }
+}
